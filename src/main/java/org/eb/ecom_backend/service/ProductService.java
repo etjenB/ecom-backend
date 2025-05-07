2f013073fa6c1c004f4b56047f086b7d0631c2ac
etjen <buljubasicetjen4@gmail.com>
2025-05-07 18:00:40 +0200

    Initial commit

---
+++ src/main/java/org/eb/ecom_backend/service/ProductService.java
@@ -0,0 +1,33 @@
+package org.eb.ecom_backend.service;
+
+import org.eb.ecom_backend.model.Product;
+import org.eb.ecom_backend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepository.save(product);
+    }
+}
